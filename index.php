f67f7db3672f406d08237118fa126d369fbf8b86
Angela Banuelos <banuelosangela56@gmail.com>
2015-04-24 22:18:48 -0700

    Coded video 18

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
 	<title>Angela's Simple To-Do list</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
 </head>
 <body>
 	<div class="wrap">
@@ -20,8 +23,8 @@
 
 							echo '<li>
 							<span>'.$task_name. '</span>
-+							<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
-+							</li>';
+							<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+						</li>';
 						}
 					}
 				}
@@ -34,15 +37,15 @@
 </form>
 </div>
 </body>
-+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-+<script>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
 	add_task();
 	function add_task(){
 		$('.add-new-task').submit(function() {
 			var new_task = $('.add-new-task input[name=new-task]').val();
 			if (new_task != '') {
 				$.post('includes/add-task.php', {task: new_task}, function(data) {
-+					$('add-new-task input[name=new-task]').val();
+					$('add-new-task input[name=new-task]').val();
 					$(data).appendTo('.task-list ul').hide().fadeIn();
 				});
 			}
