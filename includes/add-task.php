e2f8260224c6f71cca058f87c8cc4f6b5b2cb7d3
Angela Banuelos <banuelosangela56@gmail.com>
2015-04-26 21:22:36 -0700

    Got button to finally work!

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,12 +1,12 @@
-+<?php
+<?php
 $task = strip_tags($_POST['task']);
-$date = date['Y-m-d'];
-$time = date['H:i:s'];
+$date = date('Y-m-d');
+$time = date('H:i:s');
 
 include('connect.php');
 
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
-$mysqli->("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+$mysqli->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
 
 $query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
 if ($result = $mysqli->query($query)) {
---
+++ register.php
@@ -0,0 +1,5 @@
+<?php
+//it calls header, register, and foote
+require_once(__DIR__ . "/view/header.php");
+require_once(__DIR__ . "/view/register-form.php");
+require_once(__DIR__ . "/view/footer.php");
